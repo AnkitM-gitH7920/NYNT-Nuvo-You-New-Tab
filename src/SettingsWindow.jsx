a9c2555c1ea48f963ffeb7a22a754729b9e72d3c
AnkitM-gitH7920 <ankitmehra7920@gmail.com>
2026-05-30 18:26:20 +0530

    added settings

--- src/SettingsWindow.jsx
+++ src/SettingsWindow.jsx
@@ -1,12 +1,107 @@
 import "./settings-window.css";
-import { X } from "lucide-react";
+import ToggleButton from "./ToggleButton";
+import { X, ChevronDown } from "lucide-react";
+import { useRef, useState, useEffect } from "react";
 
-export default function SettingsWindow({ closeSettings }) {
+export default function SettingsWindow() {
+     // useRef
+     const chevronRef = useRef(null);
+
+     // useState
+     const [isAccordionExpanded, setIsAccordionExpanded] = useState(false);
+
+
+     let settings = [
+          {
+               category: "Customizations",
+               items: [
+                    {
+                         "settingName": "Quotes",
+                         "settingsDescription": "Daily motivational quotes",
+                         "settingFunction": () => {
+                              localStorage.setItem("dailyQuotes", false);
+                         }
+                    },
+                    {
+                         "settingName": "24-Hour Format",
+                         "settingsDescription": "Toggle 24 hours clock format",
+                         "settingFunction": () => {
+                              localStorage.setItem("clock-format", "12hour")
+                         }
+                    }
+               ]
+
+          }
+     ]
+
+     useEffect(() => {
+          console.log(settings)
+
+     }, [])
+
+
+
      return (
           <div className="settings-window">
-                    <div className="sw-head">
-                         Settings
+               <div className="sw-accordion">
+                    <div className="sw-accordion-head alignC">
+                         <span className="sw-accordion-head-title">
+                              Customization
+                         </span>
+                         <button ref={chevronRef} onClick={() => {
+                              chevronRef.current.classList.contains("chevron-rotate")
+                                   ? (() => {
+                                        chevronRef.current.classList.remove("chevron-rotate");
+                                        setIsAccordionExpanded(false);
+                                   })()
+                                   : (() => {
+                                        chevronRef.current.classList.add("chevron-rotate");
+                                        setIsAccordionExpanded(true);
+                                   })()
+                         }
+                         } className="sw-accordion-chevron center">
+                              <ChevronDown size={25} />
+                         </button>
                     </div>
+                    <div className={`sw-accordion-body alignC ${isAccordionExpanded ? "accordion-expanded" : ""}`}>
+                         <div className="single-setting alignC">
+                              <div style={{ flexDirection: "column" }} className="setting-desc">
+                                   <p>AI Tools</p>
+                                   <p>Show shortcuts for AI tools</p>
+                              </div>
+                              <ToggleButton />
+                         </div>
+                         <div className="single-setting alignC">
+                              <div style={{ flexDirection: "column" }} className="setting-desc">
+                                   <p>AI Tools</p>
+                                   <p>Show shortcuts for AI tools</p>
+                              </div>
+                              <ToggleButton />
+                         </div>
+                         <div className="single-setting alignC">
+                              <div style={{ flexDirection: "column" }} className="setting-desc">
+                                   <p>AI Tools</p>
+                                   <p>Show shortcuts for AI tools</p>
+                              </div>
+                              <ToggleButton />
+                         </div>
+                         <div className="single-setting alignC">
+                              <div style={{ flexDirection: "column" }} className="setting-desc">
+                                   <p>AI Tools</p>
+                                   <p>Show shortcuts for AI tools</p>
+                              </div>
+                              <ToggleButton />
+                         </div>
+                         <div className="single-setting alignC">
+                              <div style={{ flexDirection: "column" }} className="setting-desc">
+                                   <p>AI Tools</p>
+                                   <p>Show shortcuts for AI tools</p>
+                              </div>
+                              <ToggleButton />
+                         </div>
+                    </div>
+               </div>
           </div>
      )
 }
+// Start by adding settings into the list 
--- src/ToggleButton.jsx
+++ src/ToggleButton.jsx
@@ -1,17 +1,8 @@
 import "./toggle-btn.css";
 
-export default function ToggleButton({ defaultChecked, onChange }) {
+export default function ToggleButton() {
 
      return (
-          <>
-               <label className="switch">
-                    <input
-                         style={{ display: "none" }}
-                         checked={defaultChecked}
-                         onChange={(e) => onChange?.(e.target.checked)}
-                         type="checkbox" />
-                    <span className="toggle"></span>
-               </label >
-          </>
+          <input type="checkbox" role="switch" class="liquid-3" />
      );
 }
